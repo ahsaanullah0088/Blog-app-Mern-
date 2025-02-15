9c36bee79e3db9a728475468847894d9e2b82a84
Ahsaan Ullah <ahsaanu70@gmail.com>
2025-02-15 16:55:51 +0500

    created error handler

--- api/index.js
+++ api/index.js
@@ -21,3 +21,14 @@ console.log('Server is running on port 3000');
 
 app.use('/api/user' , userRoute);
 app.use('/api/auth' , authRoute);
+
+app.use((err , req , res , next)=>{
+    const statusCode  = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+        success : false,
+        statusCode , 
+        message
+    });
+    
+})
